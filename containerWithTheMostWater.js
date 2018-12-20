1f621997d248a9656d4b0838716fbef0ece9006d
Matthew Jones <mjones61692@gmail.com>
2018-12-20 17:53:09 -0500

    optimized containerWithTheMostWater with two pointers

--- containerWithTheMostWater.js
+++ containerWithTheMostWater.js
@@ -15,16 +15,34 @@ Output: 49
 */
 
 function container(arr) {
+  // // brute force
+  // let max = 0;
+  // arr.forEach((h1, i1) => {
+  //   arr.slice(i1).forEach((h2, i2) => {
+  //     let area = Math.min(h1, h2) * (i2);
+  //     if (area > max) {
+  //       max = area;
+  //     }
+  //   });
+  // });
+  // return max;
+
+  // optimized
   let max = 0;
-  arr.forEach((h1, i1) => {
-    arr.slice(i1).forEach((h2, i2) => {
-      let area = Math.min(h1, h2) * (i2);
-      if (area > max) {
-        max = area;
-      }
-    });
-  }); 
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let area = Math.min(arr[left], arr[right]) * (right - left);
+    if (area > max) {
+      max = area;
+    }
+    if (arr[left] > arr[right]) {
+      right--;
+    } else {
+      left++;
+    }
+  }
   return max;
 };
 
-console.log(container([1, 8, 6, 2, 5, 4, 8, 3, 7])); // 49
+console.log(container([1, 8, 6, 7, 8, 4, 3, 5, 5, 6, 9, 2, 4, 3, 2, 1, 5, 3, 4, 6, 7, 8, 9, 4, 3])); // 49
